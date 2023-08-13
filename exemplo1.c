de1316b76586a18108f6b66d5510803c2dcba099
Bia <126372529+WhiteCat2089@users.noreply.github.com>
2023-08-13 13:09:01 +0000

    Formatação

--- exemplo1.c
+++ exemplo1.c
@@ -10,7 +10,7 @@ int main() {
 	printf("Mackenzie - FCI - Fundada em 1970\n");
 	printf("Ciência da Computação\n");
 	printf("Algoritmos e Programação II\n");
-	printf("Turma 02N11 \n");
+	printf("Turma 02N11\n");
     printf("Linguagem C\n");
 	return 0;
 }
