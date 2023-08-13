eff5b1025530550648927f52d2e28e390603404f
Bia <126372529+WhiteCat2089@users.noreply.github.com>
2023-08-13 09:53:29 -0300

    Create exemplo1.c

---
+++ exemplo1.c
@@ -0,0 +1,16 @@
+/* 
+*Lab. 1
+*Exemplo 1
+*/
+
+#include <stdio.h>
+
+int main() {
+	//imprime algumas mensagems na tela
+	printf("Mackenzie - FCI\n");
+	printf("Algoritmos e ");
+	printf("Prog. II\n");
+	printf("Turma 02N11 \n");
+  printf("Linguagem C\n");
+	return 0;
+}
